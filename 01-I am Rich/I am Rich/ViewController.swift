3784f4a3cb3bad888cec34204713fa02001deb43
Aleksandra <94380380+AleksandraSRB@users.noreply.github.com>
2024-05-08 16:52:43 +0200

    Add files via upload

---
+++ 01-I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Aleksandra Sobot on 14.11.21..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
